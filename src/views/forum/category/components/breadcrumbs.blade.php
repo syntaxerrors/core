339743bb92b536026ab22b5ffea6c74ccb1891c6
Stygian <stygian.warlock.v2@gmail.com>
2014-03-06 16:28:59 -0600

    Moving forums to their own package. adding gulp

--- src/views/forum/category/components/breadcrumbs.blade.php
+++
@@ -1,6 +0,0 @@
-<small>
-	<ul class="breadcrumb">
-		<li>{{ HTML::link('/forum', 'Forums') }}</li>
-		<li class="active">{{ $category->name }}</li>
-	</ul>
-</small>
--- src/views/forum/post/components/tabcontent.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-	<div class="col-md-2">
-		@include('forum.post.components.user')
-	</div>
-	<div class="col-md-9">
-		@include('forum.post.components.'. $component)
-	</div>
-</div>
