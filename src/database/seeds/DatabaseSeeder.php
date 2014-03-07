e9dd0e8f2f0be0a5586717dbd35c128341d68c84
Stygian <stygian.warlock.v2@gmail.com>
2014-03-07 11:27:59 -0600

    Adding database folder

---
+++ src/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+class DatabaseSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Eloquent::unguard();
+
+		$this->call('UsersTableSeeder');
+		$this->call('RolesTableSeeder');
+		$this->call('ActionsTableSeeder');
+		$this->call('ActionRolesTableSeeder');
+		$this->call('RoleUsersTableSeeder');
+		$this->call('Message_typesTableSeeder');
+		$this->call('MessageFoldersTableSeeder');
+		$this->call('PreferencesTableSeeder');
+		$this->call('Preferences_usersTableSeeder');
+		$this->call('PreferencesTableSeeder_Update_01_09_2014');
+		$this->call('PreferencesTableSeeder_Update_01_21_2014');
+		$this->call('PreferencesTableSeeder_Update_11_18_2013');
+		$this->call('PreferencesTableSeeder_Update_11_20_2013');
+		$this->call('PreferencesTableSeeder_Update_12_17_2013');
+	}
+
+}
